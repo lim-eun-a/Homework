86332cdddf5f5297e8bc8a02e4ce449e1bbe76fc
DESKTOP-UIBS1SF\javastudy <limeunas2@gmail.com>
2023-12-26 11:10:26 +0900

    과제1

--- src/library.java
+++ src/library.java
@@ -23,6 +23,7 @@ public class library
 
 			con = DriverManager.getConnection(url,id,pass);
 			System.out.println("Oracle 연결성공!");
+			System.out.println();
 		}
 		catch(Exception e) {
 			System.out.println("Oracle 연결시 예외발생");
@@ -40,19 +41,232 @@ public class library
 	public void showMenu()
 	{
 		System.out.println("┌────<메뉴 선택>────┐");
-		System.out.println("│1.    책 등록      │");
-		System.out.println("│2.   도서 조회     │");
-		System.out.println("│3.전체 리스트 조회 │");
-		System.out.println("│4. 낡은 책 버리기  │");
+		System.out.println("│1.   회원 관리     │");
+		System.out.println("│2.   도서 관리     │");
+		System.out.println("│3.   도서 대여     │");
+		System.out.println("│4.  프로그램 종료  │");
+		System.out.println("└───────────────────┘");
+		System.out.print("메뉴를 선택해 주세요 : ");
+	}
+	
+	public void doRun() {
+		while(true) {
+			showMenu();
+			int choice = sc.nextInt();
+			sc.nextLine();
+			switch(choice) {
+			case 1:
+				MemberRun();
+				break;
+			case 2:
+				BookRun();
+				break;
+			case 3:
+				System.out.println("시스템 작업 중입니다. 다른 메뉴를 선택해 주세요.");
+				break;
+			case 4:
+				System.out.println("도서 프로그램을 종료합니다. 감사합니다:) ");
+				return;
+			default:
+				System.out.println("잘못 입력하셨습니다.");
+				break;	
+			}
+		}
+	}
+	
+	
+	// 회원 메뉴 선택
+	public void MemberMenu()
+	{
+		System.out.println("┌────[메뉴 선택]────┐");
+		System.out.println("│1.   회원 등록     │");
+		System.out.println("│2.   회원 조회     │");
+		System.out.println("│3. 모든 회원 조회  │");	
+		System.out.println("│4.   회원 탈퇴     │");
 		System.out.println("│5.      종료       │");
 		System.out.println("└───────────────────┘");
 		System.out.print("메뉴를 선택해 주세요 : ");
 	}
 	
-	// 프로그램 실행
-	public void doRun() {
+	// 회원 프로그램 실행
+	public void MemberRun() {
 		while(true) {
-			showMenu();
+			MemberMenu();
+			int choice = sc.nextInt();
+			sc.nextLine();
+			switch(choice) {
+			case 1:
+				addMember();
+				break;
+			case 2:
+				searchMember();
+				break;
+			case 3:
+				allMember();
+				break;
+			case 4:
+				delMember();
+				break;
+			case 5:
+				System.out.println("회원 프로그램을 종료합니다. 감사합니다:) ");
+				return;
+			default:
+				System.out.println("잘못 입력하셨습니다.");
+				break;	
+			}
+		}
+	}
+	
+	// 1-1. 회원 등록
+	public void addMember() {
+		System.out.print("등록할 회원의 아이디를 입력해 주세요 : ");
+		String id = sc.nextLine();
+			try{	
+			String sql = "select * from memberDB where id = ?";
+			psmt = con.prepareStatement(sql);
+			psmt.setString(1, id);
+			rs = psmt.executeQuery();
+			int nResult = 0;
+			while(rs.next()) {
+				nResult++;
+				rs.getString("id");
+				System.out.println(rs.getString("id")+"는 이미 사용중인 ID입니다.");
+				System.out.println("");
+			}
+			if (nResult == 0)
+			{
+				System.out.println(id+"는 사용 가능한 ID입니다.");
+				System.out.println("");
+				System.out.print("등록할 회원의 이름을 입력해 주세요 : ");
+				String name = sc.nextLine();
+				System.out.print("등록할 회원의 블랙리스트 여부를 입력해 주세요.");
+				System.out.println("(블랙리스트가 아닐 경우 엔터키를 눌러주세요)");
+				String blackuser = sc.nextLine();
+				try
+				{
+					String sql2 = "INSERT INTO MEMBERDB VALUES(seq_member_num.nextval, ?, ?, ?)";
+					psmt = con.prepareStatement(sql2);
+					
+					psmt.setString(1, id);
+					psmt.setString(2, name);
+					psmt.setString(3, blackuser);
+					
+					int updateCount = psmt.executeUpdate();
+				if(updateCount == 1) {
+					System.out.println(name + "님의 회원가입이 정상 처리 되었습니다.");
+					System.out.println("가입을 축하드립니다:)");
+				}else {
+					System.out.println("데이터 입력에 실패했습니다.(#가입오류)");
+				}
+				//System.out.println("insertCount : " + updateCount);
+			
+				}catch(Exception e)
+				{
+					e.printStackTrace();
+					System.out.println("데이터 입력에 실패했습니다.(#데이터예외)");
+				}
+			}
+		}catch(Exception e) {
+			e.printStackTrace();
+			System.out.println("입력에 실패했습니다.(#3)");
+		}
+		
+	}
+	
+	// 1-2. 회원 조회
+	public void searchMember() {
+		System.out.print("조회할 회원의 이름을 입력해 주세요 : ");
+		String name = sc.nextLine();
+		try{	
+			String sql = "select * from memberDB where name = ?";
+			
+			psmt = con.prepareStatement(sql);
+			psmt.setString(1, name);
+			rs = psmt.executeQuery();
+			
+			int nResult = 0;
+			while(rs.next()) {
+				nResult++;
+				rs.getString("id");
+				System.out.println(rs.getString("name")+"님 환영합니다.");
+			}
+			if (nResult == 0)
+			{
+				System.out.println("이름이 잘못 입력되었거나 등록되어 있지 않은 회원입니다.");
+			}
+			
+			while(rs.next()) {
+				System.out.println("------------------------------");
+				System.out.println("회  원  번  호 : " + rs.getInt("membernum"));
+				System.out.println("아    이    디 : " + rs.getString("id"));
+				System.out.println("이          름 : " + rs.getString("name"));
+				System.out.println("블랙리스트 여부: " + rs.getString("blackuser"));				
+				System.out.println("------------------------------");
+			}
+		}catch(Exception e) {
+			e.printStackTrace();
+			System.out.println("데이터 입력에 실패했습니다.(#3)");
+		}
+	}
+	
+	// 1-3. 전체 회원 조회
+	public void allMember() {
+		try{	
+			String sql = "select * from memberDB order by membernum";
+			psmt = con.prepareStatement(sql);
+			rs = psmt.executeQuery(sql);
+		
+			while(rs.next()) {
+				System.out.println("------------------------------");
+				System.out.println("회  원  번  호 : " + rs.getInt("membernum"));
+				System.out.println("아    이    디 : " + rs.getString("id"));
+				System.out.println("이          름 : " + rs.getString("name"));
+				System.out.println("블랙리스트 여부: " + rs.getString("blackuser"));				
+				System.out.println("------------------------------");
+			}
+		
+		}catch(Exception e) {
+			e.printStackTrace();
+		}
+	}
+	
+	// 1-4. 회원 탈퇴
+	public void delMember() {
+		System.out.print("탈퇴할 회원의 이름을 입력해 주세요 : ");
+		String name = sc.nextLine();
+		try{	
+			String sql = "delete from memberDB where name = ?";
+			
+			psmt = con.prepareStatement(sql);
+			psmt.setString(1, name);
+			psmt.executeUpdate();
+			
+			System.out.println(name + "(이)가 삭제되었습니다.");
+			System.out.println("이용해 주셔서 감사합니다:)");
+			
+		}catch(Exception e) {
+			System.out.println("데이터베이스 삭제 에러입니다.");
+		}
+	}
+		
+	
+	// 책 관리 메뉴 선택
+	public void BookMenu()
+	{
+		System.out.println("┌────<메뉴 선택>────┐");
+		System.out.println("│1.    책 등록      │");
+		System.out.println("│2.   도서 조회     │");
+		System.out.println("│3.전체 리스트 조회 │");
+		System.out.println("│4. 낡은 책 버리기  │");
+		System.out.println("│5.  프로그램 종료  │");
+		System.out.println("└───────────────────┘");
+		System.out.print("메뉴를 선택해 주세요 : ");
+	}
+	
+	// 책 프로그램 실행
+	public void BookRun() {
+		while(true) {
+			BookMenu();
 			int choice = sc.nextInt();
 			sc.nextLine();
 			switch(choice) {
@@ -69,7 +283,7 @@ public class library
 				delBook();
 				break;
 			case 5:
-				System.out.println("프로그램을 종료합니다. 감사합니다:) ");
+				System.out.println("도서 관리 프로그램을 종료합니다. 감사합니다:) ");
 				return;
 			default:
 				System.out.println("잘못 입력하셨습니다.");
@@ -78,7 +292,7 @@ public class library
 		}
 	}
 	
-	// 1. 책 등록
+	// 2-1. 책 등록
 	public void addBook() {
 		try
 		{
@@ -103,7 +317,7 @@ public class library
 		}
 	}
 	
-	// 2. 책 조회
+	// 2-2. 책 조회
 	public void searchBook() {
 		System.out.print("조회할 책 제목을 입력해 주세요 : ");
 		String bookname = sc.nextLine();
@@ -127,7 +341,7 @@ public class library
 		}
 	}
 	
-	// 3. 전체 리스트 조회
+	// 2-3. 전체 리스트 조회
 	public void allBook() {
 		try{	
 			String sql = "select * from bookDB order by booknum";
@@ -147,7 +361,7 @@ public class library
 		}
 	}
 	
-	// 4. 낡은 책 버리기
+	// 2-4. 낡은 책 버리기
 	public void delBook() {
 		System.out.print("삭제할 책 제목을 입력해 주세요 : ");
 		String bookname = sc.nextLine();
